6e581a9da50e3c9222616ffee5ef66d9deb2372f
unknown <racheleantonio@outllok.com>
2019-12-01 11:27:31 +0100

    migrated from Bitbucket

---
+++ project1/android-app/app/src/main/java/com/company/eventify/utilities/MyAdapter.java
@@ -0,0 +1,22 @@
+package com.company.eventify.utilities;
+
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class MyAdapter extends RecyclerView.Adapter {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
